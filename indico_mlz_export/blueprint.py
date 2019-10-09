70b46bb257413ceb5ef271f16884af65d9e33174
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2019-10-09 09:24:59 +0200

    Adjust bp name

--- indico_mlz_export/blueprint.py
+++ indico_mlz_export/blueprint.py
@@ -30,7 +30,7 @@ from __future__ import unicode_literals
 from indico.core.plugins import IndicoPluginBlueprint
 from indico_mlz_export.controller import RHExportRegistrations, RHExportRegistration, RHExportRegistrationsFlat, RHExportRegistrationFlat
 
-blueprint = IndicoPluginBlueprint('mlzexport', __name__, url_prefix='/mlz/export')
+blueprint = IndicoPluginBlueprint('mlz_export', __name__, url_prefix='/mlz/export')
 # API
 blueprint.add_url_rule(
     '/<int:event_id>/registrants/<int:registrant_id>', 'api_registrant', RHExportRegistration, methods=('GET', ))
